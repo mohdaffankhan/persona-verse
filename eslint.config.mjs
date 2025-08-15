d88219722aaa92650167bcc7625085d254b99a09
Affan Khan <mohdaffankhan@users.noreply.github.com>
2025-08-15 16:45:47 +0530

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["app/generated/**"],
+    ignores: ["lib/generated/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript", {
     rules: {
